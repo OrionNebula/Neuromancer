8741130de319c0eba517dc4ad750d56a8a782e78
Andrew Trewin <zancoproductions@gmail.com>
2013-05-13 20:01:45 -0500

    Wikipedia SVG convert
    SVG files now autoconvert to JPG for analysis

--- eclipse/Neuromancer/src/AlphaAPISample.java
+++ eclipse/Neuromancer/src/AlphaAPISample.java
@@ -1,10 +1,22 @@
+import java.awt.image.*;
 import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Random;
 
+import javax.imageio.*;
 import javax.sound.sampled.AudioFileFormat;
+import javax.swing.*;
 
 import com.wolfram.alpha.*;
 import com.darkprograms.speech.microphone.*;
 import com.darkprograms.speech.recognizer.*;
+
+import org.w3c.dom.Document;
+import org.wikipedia.*;
+import org.apache.batik.transcoder.*;
+import org.apache.batik.transcoder.image.JPEGTranscoder;
+import org.apache.batik.transcoder.svg2svg.SVGTranscoder;
 
 public class AlphaAPISample {
 	
@@ -52,10 +64,7 @@ public class AlphaAPISample {
         
         query.setInput(input);
         
-        try {
-            System.out.println("Query URL:");
-            System.out.println(engine.toURL(query));
-            System.out.println("");
+        /*try {
             
             WAQueryResult queryResult = engine.performQuery(query);
             
@@ -86,7 +95,59 @@ public class AlphaAPISample {
             }            
         } catch (WAException e) {
             e.printStackTrace();
+        }*/
+        
+        Wiki theWiki = new Wiki("en.wikipedia.org");
+        try {
+			System.out.println(theWiki.getSectionText(input,0).replace("[", "").replace("]", "").replace("{", "").replace("}", ""));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        InputStream is = null;
+        String theImage = null;
+		try {
+			theImage = theWiki.getImagesOnPage(input)[1];
+			is = new ByteArrayInputStream(theWiki.getImage(theImage));
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        try{
+        	BufferedImage image = null;
+        	System.out.println(theImage.substring(theImage.length()-3, theImage.length()));
+        	if(theImage.endsWith("svg")){
+        		image = svgToPNG(is);
+        	}else{
+        		image = ImageIO.read(is);}
+            JOptionPane.showMessageDialog(null, "", "", JOptionPane.INFORMATION_MESSAGE, new ImageIcon(image));
+        } catch(Exception e){
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Failure", JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
         }
+    }
+    
+    public static BufferedImage svgToPNG(InputStream source) throws Exception
+    {
+    	// Create a JPEG transcoder
+        JPEGTranscoder t = new JPEGTranscoder();
+
+        // Set the transcoding hints.
+        t.addTranscodingHint(JPEGTranscoder.KEY_QUALITY,
+                             new Float(.8));
+
+        // Create the transcoder input.
+        TranscoderInput input = new TranscoderInput(source);
+
+        // Create the transcoder output.
+        OutputStream ostream = new FileOutputStream("out.jpg");
+        TranscoderOutput output = new TranscoderOutput(ostream);
+        // Save the image.
+        t.transcode(input, output);
+
+        // Flush and close the stream.
+        ostream.flush();
+        ostream.close();
+        return ImageIO.read(new FileInputStream("out.jpg"));
     }
 
 }
